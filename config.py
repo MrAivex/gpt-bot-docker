7aaf9da02d3ace959420aaf6f7863b0bcf1cb8d9
GrafAleksey315 <lekstar_67@mail.ru>
2026-04-29 19:22:33 +0300

    8

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 TOKEN = "f9LHodD0cOKpRm54dQO2ji8dNZcUtkJSpeFRWkP98hwIPZO3R9PtiSg759pxVK7jWQNNCePOp5eldQ8_MpDN"
 
 # локальный запуск
-DB_DSN = "postgresql://postgres:NoForgot_938@localhost:5432/gpt_bot_db"
+# DB_DSN = "postgresql://postgres:NoForgot_938@localhost:5432/gpt_bot_db"
 
 # запуск на серваке
-# DB_DSN = "postgresql://postgres:NoForgot_938@db:5432/gpt_bot_db"
+DB_DSN = "postgresql://postgres:NoForgot_938@db:5432/gpt_bot_db"
 
 WEBHOOK_PATH = "/max-webhook"
 ADMIN_ID = 5787551
@@ -16,7 +16,7 @@ SHOP_ID="1342218"
 SUPPORT_LINK="https://max.ru/u/f9LHodD0cOIdennqYfdEQFtEvXCDlTkAAm3lFWmfN4GgG94KMLS2FqXsgr8"
 
 # локальный запуск
-WEBHOOK_URL=f"https://my-super-gpt-bot.loca.lt{WEBHOOK_PATH}"
+# WEBHOOK_URL=f"https://my-super-gpt-bot.loca.lt{WEBHOOK_PATH}"
 
 # запуск на серваке
-# WEBHOOK_URL=f"https://max-gpt-ai-helper-bot.ru{WEBHOOK_PATH}"
+WEBHOOK_URL=f"https://max-gpt-ai-helper-bot.ru{WEBHOOK_PATH}"
